0ffe5e20a95e53f4cb3853e6e908249e5cf070fa
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-03-19 22:49:53 -0500

    Completed app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./Image";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      src:
+        "https://images.pexels.com/photos/116024/pexels-photo-116024.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+    };
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Image</h2>
+        <Image src={this.state.src} />
       </div>
     );
   }
